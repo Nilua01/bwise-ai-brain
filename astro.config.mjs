4f6fa1ebb7d5fce1fc7264c8db4404bbed1485db
Dr. Nirmal Acharya <itsdrnirmal@gmail.com>
2026-04-21 03:55:57 +1000

    fix: install react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,7 @@
-import { defineConfig } from "astro/config"; import tailwind from "@astrojs/tailwind"; export default defineConfig({ integrations: [tailwind()] });
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+export default defineConfig({
+  integrations: [tailwind(), react()],
+});
